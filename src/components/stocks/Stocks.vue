b6526d5750d08a1e3fc3a1e0be9dd5ba3c18a832
Bruno Schubert Medeiros <BSCHUBERTM@edu.unisinos.br>
2018-12-03 17:54:37 -0200

    REfactored Stock estructure

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,16 +8,6 @@
 <script>
     import Stock from './Stock.vue';
     export default {
-        data() {
-            return {
-                stocks: [
-                    {id:1, name: 'Honda', price: 110},
-                    {id:2, name: 'Google', price: 200},
-                    {id:3, name: 'Facebook', price: 150},
-                    {id:4, name: 'Apple', price: 190},
-                ]
-            }
-        },
         components: {
             appStock: Stock
         }
