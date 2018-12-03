443c58f70246534e96c2aefc840b6c68ddf7bafa
Bruno Schubert Medeiros <BSCHUBERTM@edu.unisinos.br>
2018-12-03 17:21:01 -0200

    Add Stock Presentation

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>stocks - Stocks Component</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :key="stock">
+        </app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id:1, name: 'Honda', price: 110},
+                    {id:2, name: 'Google', price: 200},
+                    {id:3, name: 'Facebook', price: 150},
+                    {id:4, name: 'Apple', price: 190},
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
